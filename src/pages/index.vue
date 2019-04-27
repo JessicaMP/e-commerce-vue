d68230be50f1103db51aa0d233cabf5166bf81ee
JessicaMP19 <jmanturano@americatv.pe>
2019-04-27 11:31:13 -0500

    add vuex

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,35 @@
 <template>
-    <Header/>
+    <div>
+        <Header/>
+        <v-carousel
+            delimiter-icon="stop"
+            prev-icon="arrow_left"
+            next-icon="arrow_right"
+        >
+            <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            ></v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 <script>
 import Header from '@/components/Header.vue'
+import { mapState } from 'vuex'
 
 export default {
     components: {
         Header
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+        ...mapState({
+            items: state => state.imagesCarousel
+        })
     }
 }
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        imagesCarousel: [
+            {
+                src: 'https://images.pexels.com/photos/164501/pexels-photo-164501.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+            {
+                src: 'https://images.pexels.com/photos/1345082/pexels-photo-1345082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+            {
+                src: 'https://images.pexels.com/photos/1534517/pexels-photo-1534517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+            {
+                src: 'https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            }
+        ]
+    }
+});
